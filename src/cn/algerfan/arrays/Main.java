53f65cc8515225eadb51b78b750463e66314cde2
AlgerFan <fsj.wlgzs@gmail.com>
2019-08-28 09:48:11 +0800

    封装自己的数组

---
+++ src/cn/algerfan/arrays/Main.java
@@ -0,0 +1,25 @@
+package cn.algerfan.arrays;
+
+/**
+ * @author algerfan
+ * @time 2019/8/25 21: 16
+ */
+public class Main {
+    public static void main(String[] args) {
+        Array<Integer> array = new Array<>(20);
+        for (int i = 0; i < 10; i++) {
+            array.addLast(i);
+        }
+
+        array.add(2, 100);
+        System.out.println(array);
+        array.addLast(-1);
+        System.out.println(array);
+
+        array.remove(2);
+        array.removeElement(4);
+        System.out.println(array);
+        array.removeFirst();
+        System.out.println(array);
+    }
+}
